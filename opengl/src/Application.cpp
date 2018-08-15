7c90570a4314dda6cb01ed5ae4dedbfece449896
HappyFaceFriend <yjl980730@gmail.com>
2018-08-15 13:40:23 +0900

    예제 완성

--- opengl/src/Application.cpp
+++ opengl/src/Application.cpp
@@ -5,12 +5,28 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+	x;\
+	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 struct ShaderSource
 {
 	std::string VertexSource;
 	std::string FragmentSource;
 };
-
+static void GLClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+static bool GLLogCall(const char* function, const char *file, int line)
+{
+	while (GLenum error = glGetError())
+	{
+		std::cout <<function<<"/"<<file<<":"<<line <<error<< std::endl;
+		return false;
+	}
+	return true;
+}
 static ShaderSource ParseShader(const std::string filepath)
 {
 	enum class ShaderType{
@@ -41,36 +57,37 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
 	unsigned int id = glCreateShader(type);
 	const char* src = source.c_str();
-	glShaderSource(id, 1, &src, nullptr);
-	glCompileShader(id);
+	GLCall(glShaderSource(id, 1, &src, nullptr));
+	GLCall(glCompileShader(id));
 	
 	int result;
-	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
 	if (result == GL_FALSE)
 	{
 		int length; 
-		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
 		char* message = (char*)alloca(length * sizeof(char));
-		glGetShaderInfoLog(id, length, &length, message);
+		GLCall(glGetShaderInfoLog(id, length, &length, message));
 		std::cout << message << std::endl;
-		glDeleteShader(id);
+		GLCall(glDeleteShader(id));
 		return 0;
 	}
 	return id;
 }
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& framentShader)
 {
-	unsigned int program=glCreateProgram();
+	unsigned int program;
+	GLCall(program= glCreateProgram());
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
 	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, framentShader);
 
-	glAttachShader(program, vs);
-	glAttachShader(program, fs);
-	glLinkProgram(program);
-	glValidateProgram(program);
+	GLCall(glAttachShader(program, vs));
+	GLCall(glAttachShader(program, fs));
+	GLCall(glLinkProgram(program));
+	GLCall(glValidateProgram(program));
 
-	glDeleteShader(vs);
-	glDeleteShader(fs);
+	GLCall(glDeleteShader(vs));
+	GLCall(glDeleteShader(fs));
 
 	return program;
 }
@@ -82,8 +99,14 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	glfwSwapInterval(1);
 	if (!window)
 	{
 		glfwTerminate();
@@ -108,23 +131,41 @@ int main(void)
 		0,1,2,
 		2,3,0
 	};
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));	//이 버퍼를 그려라
+
 	unsigned int triangleBuffer;
-	glGenBuffers(1, &triangleBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, triangleBuffer);	//이 버퍼를 그려라
-	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);	//버퍼에 데이터 사빕
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0);
+	GLCall(glGenBuffers(1, &triangleBuffer));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, triangleBuffer));	//이 버퍼를 그려라
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));	//버퍼에 데이터 사빕
+	GLCall(glEnableVertexAttribArray(0));
+	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0));
+							// id,데이터수,자료형,?,1버텍스사이즈,offset
 
 
 	unsigned int indexBuffer;
-	glGenBuffers(1, &indexBuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);	//이 버퍼를 그려라
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6* sizeof(unsigned int), indicies, GL_STATIC_DRAW);	//버퍼에 데이터 사빕
+	GLCall(glGenBuffers(1, &indexBuffer));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer));	//이 버퍼를 그려라
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6* sizeof(unsigned int), indicies, GL_STATIC_DRAW));	//버퍼에 데이터 사빕
 
 	ShaderSource source = ParseShader("res/shaders/BasicShader.shader");
 
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-	glUseProgram(shader);
+
+	GLCall(glUseProgram(shader));	//bound shader
+
+	GLCall(int location = glGetUniformLocation(shader,"u_Color"));
+	ASSERT(location != -1);
+	GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+	float r = 0.0f;
+	float increment = 0.05f;
+
+
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -132,8 +173,19 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//glDrawArrays(GL_TRIANGLES, 0, 4);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,nullptr);
+		//glDrawArrays(GL_TRIANGLES, 0, 4;
+
+
+		GLCall(glUseProgram(shader));
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, triangleBuffer));	//이 버퍼를 그려라
+		r += increment;
+		if (r > 1.0f)
+			increment = -0.05f;
+		else if (r <0.0f)
+			increment = 0.05f;
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,nullptr));
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
